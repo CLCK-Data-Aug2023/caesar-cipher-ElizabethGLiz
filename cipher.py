b27c74445bb7ff9733195fe3d60c8bd79fd1de02
ELizabethGLiz <lizgeorge2019@gmail.com>
2023-11-04 10:05:03 -0400

    This is my second cipher commit

--- cipher.py
+++ cipher.py
@@ -1,7 +1,3 @@
-
-#This Caesar cipher program replaces
-#a letter in the sentence with a letter that is a five  down in the alphabet
-
 sentence = input("Please enter a sentence")
 print(sentence)
 sentence = sentence.lower()
@@ -34,9 +30,18 @@ substitution_dict = {
     "y": "d",
     "z": "e",
 }
-secret_sentence = ""
-for char in sentence:
-    if char in substitution_dict:
-        char = substitution_dict[char]
-    secret_sentence += char
-print(secret_sentence)
+secret_sentence = []
+i = 0
+for i in range(len(sentence)):
+    print(f" Each letter of the sentence {sentence[i]}")
+    if sentence[i] in substitution_dict:
+        print("yes")
+        secret_sentence.append(substitution_dict[sentence[i]])
+        i += 1
+    else:
+        print("no")
+        secret_sentence.append(sentence[i])
+        i += 1
+secret_sentence = ''.join(secret_sentence).split()
+print(*secret_sentence)
+        
